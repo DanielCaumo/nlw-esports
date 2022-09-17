5734b70124c6e5ed4a597445a7b431126cb26d30
DanielCaumo <dancaumo@gmail.com>
2022-09-16 21:07:57 -0300

    iniciando projeto

---
+++ web/src/App.tsx
@@ -0,0 +1,26 @@
+// JSX: JavaScript + XML (HTML)
+
+interface ButtonProps {
+  title: string;
+}
+
+function Button(props: ButtonProps){
+  return(
+    <button>
+      {props.title}
+    </button>
+  )
+}
+
+function App() {
+  return (
+    <div>
+      <Button title="Send 1"/>
+      <Button title="Send 2"/>
+      <Button title="Send 3"/>
+      <Button title="Hello world!"/>
+    </div>
+  ) 
+}
+
+export default App
